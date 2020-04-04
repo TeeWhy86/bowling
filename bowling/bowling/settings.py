a0bc4f97b08a33a0d943347a82e2e6b5c42ea063
tythompson86 <ty.thompson86@gmail.com>
2020-04-04 18:50:14 -0400

    update

--- bowling/bowling/settings.py
+++ bowling/bowling/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bowling.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bowling',
+        'USER': 'root',
+        'PASSWORD': 'guess666',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
